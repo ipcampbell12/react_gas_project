b7d48a54a515a4cab3284fab2d90402cd8445382
ipcampbell12 <ipcampbell12@gmail.com>
2023-05-16 12:01:54 -0700

    added some stuff

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import React from 'react';
 function App(props) {
     return (
         <div>
-            <h1>Hell to your mom!</h1>
+            <h1>Hello to your mom!</h1>
+            <div>Your mom is at her house</div>
+            <p> Everyone is at your mom's house</p>
+            <button>Doorbell</button>
         </div>
     );
 }
