cdefed31728fdc63113536c03bd78e139f91ce24
ipcampbell12 <ipcampbell12@gmail.com>
2023-05-16 11:51:47 -0700

    added broswer router

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App(props) {
     return (
         <div>
-
+            <h1>Hell to your mom!</h1>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-import { createRoot } from "react-dom/client";
-import { App } from "./App";
+import { createRoot } from 'react-dom/client';
+import App from "./App"
+import { BrowserRouter } from "react-router-dom"
 
-const container = document.getElementById("app");
-const root = createRoot(container)
-root.render(<App />);
+const container = document.getElementById("app")
+const root = createRoot(container);
+root.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>)
